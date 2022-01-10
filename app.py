cc78f06a0bfea2941d6cdd7f347d619d53cd5cc9
Pawel Bak <pawelbak300589@gmail.com>
2022-01-10 18:39:18 +0000

    Refactor all resources

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -24,76 +24,10 @@ def create_tables():
 jwt = JWTManager(app)
 
 
-@jwt.additional_claims_loader
-def add_claims_to_jwt(
-    identity
-):  # Remember identity is what we define when creating the access token
-    if (
-        identity == 1
-    ):  # instead of hard-coding, we should read from a file or database to get a list of admins instead
-        return {"is_admin": True}
-    return {"is_admin": False}
-
-
 # This method will check if a token is blocklisted, and will be called automatically when blocklist is enabled
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blocklist(jwt_header, jwt_payload):
-    return (
-        jwt_payload["jti"] in BLOCKLIST
-    )  # Here we blocklist particular JWTs that have been created in the past.
-
-
-# The following callbacks are used for customizing jwt response/error messages.
-# The original ones may not be in a very pretty format (opinionated)
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_payload):
-    return jsonify({"message": "The token has expired.", "error": "token_expired"}), 401
-
-
-@jwt.invalid_token_loader
-def invalid_token_callback(
-    error
-):  # we have to keep the argument here, since it's passed in by the caller internally
-    return (
-        jsonify(
-            {"message": "Signature verification failed.", "error": "invalid_token"}
-        ),
-        401,
-    )
-
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return (
-        jsonify(
-            {
-                "description": "Request does not contain an access token.",
-                "error": "authorization_required",
-            }
-        ),
-        401,
-    )
-
-
-@jwt.needs_fresh_token_loader
-def token_not_fresh_callback(jwt_header, jwt_payload):
-    return (
-        jsonify(
-            {"description": "The token is not fresh.",
-                "error": "fresh_token_required"}
-        ),
-        401,
-    )
-
-
-@jwt.revoked_token_loader
-def revoked_token_callback(jwt_header, jwt_payload):
-    return (
-        jsonify(
-            {"description": "The token has been revoked.", "error": "token_revoked"}
-        ),
-        401,
-    )
+    return jwt_payload["jti"] in BLOCKLIST
 
 
 api.add_resource(Store, "/store/<string:name>")
--- resources/item.py
+++ resources/item.py
@@ -1,9 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import (
-    jwt_required,
-    get_jwt,
-    get_jwt_identity,
-)
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
 
@@ -16,7 +12,6 @@ class Item(Resource):
         "store_id", type=int, required=True, help="Every item needs a store_id."
     )
 
-    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -26,10 +21,7 @@ class Item(Resource):
     @jwt_required(fresh=True)
     def post(self, name):
         if ItemModel.find_by_name(name):
-            return (
-                {"message": "An item with name '{}' already exists.".format(name)},
-                400,
-            )
+            return {"message": "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
 
@@ -44,10 +36,6 @@ class Item(Resource):
 
     @jwt_required()
     def delete(self, name):
-        claims = get_jwt()
-        if not claims["is_admin"]:
-            return {"message": "Admin privilege required."}, 401
-
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -70,25 +58,5 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    @jwt_required(optional=True)
     def get(self):
-        """
-        Here we get the JWT identity, and then if the user is logged in (we were able to get an identity)
-        we return the entire item list.
-
-        Otherwise we just return the item names.
-
-        This could be done with e.g. see orders that have been placed, but not see details about the orders
-        unless the user has logged in.
-        """
-        user_id = get_jwt_identity()
-        items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {"items": items}, 200
-        return (
-            {
-                "items": [item["name"] for item in items],
-                "message": "More data available if you log in.",
-            },
-            200,
-        )
+        return {"items": [item.json() for item in ItemModel.find_all()]}, 200
