d453b1187e1489ff0ebabad3cf9474159724e440
Pawel Bak <pawelbak300589@gmail.com>
2022-01-22 15:26:15 +0000

    Add user schema

---
+++ schemas/user.py
@@ -0,0 +1,12 @@
+from msilib.schema import Class
+from marshmallow import Schema, fields
+
+
+class UserSchema(Schema):
+    class Meta:
+        load_only = ("password",)
+        dump_only = ("id",)
+
+    id = fields.Int()
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
